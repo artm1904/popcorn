8e8df453c349948381c98b939160f1b121774da0
ваш Ðartm1904 < artmsavchenko@yandex.ru>
2025-02-21 22:50:43 +0300

    first commit

---
+++ Main/Main.cpp
@@ -0,0 +1,192 @@
+﻿// Popcorn.cpp : Defines the entry point for the application.
+//
+
+#include "Main.h"
+#include "../Engine/Engine.h"
+
+#include <Windows.h>
+#include <iostream>
+#include <minwindef.h>
+#include <windef.h>
+#include <windows.h>
+#include <wingdi.h>
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst; // current instance
+AsEngine Engine;
+WCHAR szTitle[MAX_LOADSTRING];       // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING]; // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM MyRegisterClass(HINSTANCE hInstance);
+BOOL InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
+//------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                      LPWSTR lpCmdLine, int nCmdShow) {
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  UNREFERENCED_PARAMETER(lpCmdLine);
+
+  // TODO: Place code here.
+  AsConfig::Setup_Colors();
+
+  // Initialize global strings
+  LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+  LoadStringW(hInstance, IDC_POPCORN, szWindowClass, MAX_LOADSTRING);
+  MyRegisterClass(hInstance);
+
+  // Perform application initialization:
+  if (!InitInstance(hInstance, nCmdShow)) {
+    return FALSE;
+  }
+
+  HACCEL hAccelTable =
+      LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_POPCORN));
+
+  MSG msg;
+
+  // Main message loop:
+  while (GetMessage(&msg, nullptr, 0, 0)) {
+    if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    }
+  }
+
+  return (int)msg.wParam;
+}
+
+// Регистрация класса окна
+ATOM MyRegisterClass(HINSTANCE hInstance) {
+  WNDCLASSEXW wcex;
+
+  wcex.cbSize = sizeof(WNDCLASSEX);
+
+  wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = WndProc;
+  wcex.cbClsExtra = 0;
+  wcex.cbWndExtra = 0;
+  wcex.hInstance = hInstance;
+  wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_POPCORN));
+  wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  // wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+  wcex.hbrBackground = AsConfig::BG_Brush;
+  wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_POPCORN);
+  wcex.lpszClassName = L"MainWndClass";
+  wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+  return RegisterClassExW(&wcex);
+}
+
+// Инициализация экземпляра
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
+
+  RECT window_rect;
+  window_rect.left = 0;
+  window_rect.top = 0;
+  window_rect.right = 320 * 3;
+  window_rect.bottom = 200 * 3;
+  std::cout << "window_rect.right = " << window_rect.right << std::endl;
+
+  AdjustWindowRect(&window_rect, WS_OVERLAPPEDWINDOW, TRUE);
+
+  HWND hWnd = CreateWindowW(L"MainWndClass", L"Popcorn", WS_OVERLAPPEDWINDOW, 0,
+                            0, window_rect.right - window_rect.left,
+                            window_rect.bottom - window_rect.top, nullptr,
+                            nullptr, hInstance, nullptr);
+
+  if (hWnd == 0) {
+    return FALSE;
+  }
+
+  Engine.Init_Engine(hWnd);
+
+  ShowWindow(hWnd, nCmdShow);
+  UpdateWindow(hWnd);
+
+  return TRUE;
+}
+
+//------------------------------------------------------------------------------------------------------------
+//
+
+// Обработчик сообщений окна
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam,
+                         LPARAM lParam) {
+
+  int wmId;
+  PAINTSTRUCT ps;
+  HDC hdc;
+
+  switch (message) {
+  case WM_COMMAND: {
+    wmId = LOWORD(wParam);
+    // Разобрать выбор в меню:
+    switch (wmId) {
+    case IDM_EXIT:
+      DestroyWindow(hWnd);
+      break;
+    default:
+      return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+  } break;
+  case WM_PAINT: {
+
+    hdc = BeginPaint(hWnd, &ps);
+
+    // TODO: Добавьте сюда любой код прорисовки, использующий hdc...
+
+    Engine.Draw_Frame(hdc, ps.rcPaint);
+
+    EndPaint(hWnd, &ps);
+  } break;
+
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    break;
+
+  case WM_KEYDOWN:
+
+    switch (wParam) {
+    case VK_LEFT:
+      return Engine.On_Key_Down(EKT_Left);
+    case VK_RIGHT:
+      return Engine.On_Key_Down(EKT_Right);
+    case VK_SPACE:
+      return Engine.On_Key_Down(EKT_Space);
+    }
+    break;
+
+  case WM_TIMER:
+    if (wParam == Timer_ID) {
+      return Engine.On_Timer();
+    }
+    break;
+
+  default:
+    return DefWindowProc(hWnd, message, wParam, lParam);
+  }
+  return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+  UNREFERENCED_PARAMETER(lParam);
+  switch (message) {
+  case WM_INITDIALOG:
+    return (INT_PTR)TRUE;
+
+  case WM_COMMAND:
+    if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
+      EndDialog(hDlg, LOWORD(wParam));
+      return (INT_PTR)TRUE;
+    }
+    break;
+  }
+  return (INT_PTR)FALSE;
+}
+//------------------------------------------------------------------------------------------------------------
---
+++ Platform/Platform.cpp
@@ -0,0 +1,394 @@
+﻿#include "Platform.h"
+
+// AsPlatform
+//------------------------------------------------------------------------------------------------------------
+AsPlatform::AsPlatform()
+    : X_Pos(AsConfig::Border_X_Offset), X_Step(AsConfig::Global_Scale * 2),
+      Platform_State(EPS_Normal), Inner_Width(Normal_Platform_Inner_Width),
+      Rolling_Step(0), Width(Normal_Width), Platform_Rect{},
+      Prev_Platform_Rect{}, Highlight_Pen(0), Platform_Circle_Pen(0),
+      Platform_Inner_Pen(0), Platform_Circle_Brush(0), Platform_Inner_Brush(0) {
+  X_Pos = (AsConfig::Max_X_Pos - Width) / 2;
+}
+//------------------------------------------------------------------------------------------------------------
+bool AsPlatform::Check_Hit(double next_x_pos, double next_y_pos, ABall *ball) {
+
+  double inner_top_y;
+  double inner_low_y;
+  double inner_left_x;
+  double inner_right_x;
+  double inner_y;
+
+  if (next_y_pos + ball->Radius < AsConfig::Platform_Y_Pos) {
+    return false;
+  }
+
+  // Reflect from the left edge of the platform
+  if (Reflect_On_Circle(next_x_pos, next_y_pos, 0.0, ball)) {
+    return true;
+  }
+
+  // Reflect from the right edge of the platform
+  if (Reflect_On_Circle(next_x_pos, next_y_pos, Width - Circle_Size, ball)) {
+    return true;
+  }
+
+  // The position of the center path of the platform's
+  inner_top_y = (double)(AsConfig::Platform_Y_Pos + 1);
+  inner_low_y = (double)(AsConfig::Platform_Y_Pos + Height - 1);
+  inner_left_x = (double)(X_Pos + Circle_Size - 1);
+  inner_right_x = (double)(X_Pos + Width - (Circle_Size - 1));
+  double reflection_pos = 0;
+
+  // Reflection from the center of the platform
+  //  Check if ball is need to be reflected from the center part of the
+  //  platform
+  if (ball->Is_Moving_Up()) {
+    inner_y = inner_low_y;
+  } else {
+    inner_y = inner_top_y;
+  }
+
+  // Check if the ball reflect from the bottom or top edge of the platform
+
+  if (Hit_Circle_On_Line(next_y_pos - inner_y, next_x_pos, inner_left_x,
+                         inner_right_x, ball->Radius, reflection_pos)) {
+    ball->Reflect(true);
+    return true;
+  }
+
+  return false;
+}
+
+bool AsPlatform::Reflect_On_Circle(double next_x_pos, double next_y_pos,
+                                   double fplatform_ball_x_offser,
+                                   ABall *ball) {
+
+  double dx;
+  double dy;
+  double distance;
+  double two_radius;
+
+  // angles
+  double beta;
+  double alfa;
+  double gamma;
+
+  // the center coordinates of the edges ball of the platform
+  double platform_ball_x;
+  double platform_ball_y;
+  double platform_ball_radius;
+  double related_ball_direction;
+  // double moving_step_size = AsConfig::Global_Scale * 0.05;
+
+  // The position of the edge of the platform
+  platform_ball_radius = Circle_Size / 2.;
+  platform_ball_x =
+      (double)(X_Pos + platform_ball_radius) + fplatform_ball_x_offser;
+  platform_ball_y = (double)(AsConfig::Platform_Y_Pos + platform_ball_radius);
+
+  // Reflection from the  edges of the platform
+  dx = next_x_pos - platform_ball_x;
+  dy = next_y_pos - platform_ball_y;
+
+  distance = sqrt(dx * dx + dy * dy);
+  two_radius = platform_ball_radius + ball->Radius;
+
+  if (fabs(distance - two_radius) < AsConfig::Moving_Step_Size) {
+
+    // The ball is touch one of the edges of the platform
+
+    beta = atan2(-dy, dx);
+
+    related_ball_direction = ball->Get_Direction();
+    related_ball_direction = related_ball_direction - beta;
+
+    if (related_ball_direction > 2.0 * M_PI) {
+      related_ball_direction = related_ball_direction - 2.0 * M_PI;
+    }
+
+    if (related_ball_direction < 0.) {
+      related_ball_direction = related_ball_direction + 2.0 * M_PI;
+    }
+
+    if (related_ball_direction > M_PI_2 &&
+        related_ball_direction < M_PI + M_PI_2) {
+      alfa = beta + M_PI - ball->Get_Direction();
+      gamma = beta + alfa;
+
+      ball->Set_Direction(gamma);
+      return true;
+    }
+  }
+  return false;
+}
+
+//------------------------------------------------------------------------------------------------------------
+void AsPlatform::Init() {
+  Highlight_Pen = CreatePen(PS_SOLID, 0, RGB(255, 255, 255));
+
+  AsConfig::Create_Pen_Brush(151, 0, 0, Platform_Circle_Pen,
+                             Platform_Circle_Brush);
+  AsConfig::Create_Pen_Brush(0, 128, 192, Platform_Inner_Pen,
+                             Platform_Inner_Brush);
+}
+//------------------------------------------------------------------------------------------------------------
+void AsPlatform::Act() {
+  switch (Platform_State) {
+  case EPS_Meltdown:
+  case EPS_Roll_In:
+  case EPS_Expand_Roll_In:
+    Redraw_Platform();
+  }
+}
+//------------------------------------------------------------------------------------------------------------
+EPlatform_State AsPlatform::Get_State() { return Platform_State; }
+//------------------------------------------------------------------------------------------------------------
+void AsPlatform::Set_State(EPlatform_State new_state) {
+  int i, len;
+
+  if (Platform_State == new_state)
+    return;
+
+  switch (new_state) {
+  case EPS_Meltdown:
+    len = sizeof(Meltdown_Platform_Y_Pos) / sizeof(Meltdown_Platform_Y_Pos[0]);
+
+    for (i = 0; i < len; i++)
+      Meltdown_Platform_Y_Pos[i] = Platform_Rect.bottom;
+
+    break;
+
+  case EPS_Roll_In:
+    X_Pos = AsConfig::Max_X_Pos - 1;
+    Rolling_Step = Max_Rolling_Step - 1;
+    break;
+  }
+
+  Platform_State = new_state;
+}
+//------------------------------------------------------------------------------------------------------------
+void AsPlatform::Redraw_Platform() {
+  int platform_width;
+
+  Prev_Platform_Rect = Platform_Rect;
+
+  if (Platform_State == EPS_Roll_In)
+    platform_width = Circle_Size;
+  else
+    platform_width = Width;
+
+  Platform_Rect.left = X_Pos * AsConfig::Global_Scale;
+  Platform_Rect.top = AsConfig::Platform_Y_Pos * AsConfig::Global_Scale;
+  Platform_Rect.right =
+      Platform_Rect.left + platform_width * AsConfig::Global_Scale;
+  Platform_Rect.bottom = Platform_Rect.top + Height * AsConfig::Global_Scale;
+
+  if (Platform_State == EPS_Meltdown)
+    Prev_Platform_Rect.bottom =
+        (AsConfig::Max_Y_Pos + 1) * AsConfig::Global_Scale;
+
+  InvalidateRect(AsConfig::Hwnd, &Prev_Platform_Rect, FALSE);
+  InvalidateRect(AsConfig::Hwnd, &Platform_Rect, FALSE);
+}
+//------------------------------------------------------------------------------------------------------------
+void AsPlatform::Draw(HDC hdc, RECT &paint_area) { // Рисуем платформу
+
+  RECT intersection_rect;
+
+  if (!IntersectRect(&intersection_rect, &paint_area, &Platform_Rect))
+    return;
+
+  switch (Platform_State) {
+  case EPS_Normal:
+    Draw_Normal_State(hdc, paint_area);
+    break;
+
+  case EPS_Meltdown:
+    Draw_Meltdown_State(hdc, paint_area);
+    break;
+
+  case EPS_Roll_In:
+    Draw_Roll_In_State(hdc, paint_area);
+    break;
+
+  case EPS_Expand_Roll_In:
+    Draw_Expanding_Roll_In_State(hdc, paint_area);
+    break;
+  }
+}
+//------------------------------------------------------------------------------------------------------------
+void AsPlatform::Clear_BG(HDC hdc) { // Очищаем фоном прежнее место
+
+  SelectObject(hdc, AsConfig::BG_Pen);
+  SelectObject(hdc, AsConfig::BG_Brush);
+
+  Rectangle(hdc, Prev_Platform_Rect.left, Prev_Platform_Rect.top,
+            Prev_Platform_Rect.right -1 , Prev_Platform_Rect.bottom -1 );
+}
+//------------------------------------------------------------------------------------------------------------
+void AsPlatform::Draw_Circle_Highlight(HDC hdc, int x,
+                                       int y) { // Рисуем блик на шарике
+
+  SelectObject(hdc, Highlight_Pen);
+
+  Arc(hdc, x + AsConfig::Global_Scale, y + AsConfig::Global_Scale,
+      x + (Circle_Size - 1) * AsConfig::Global_Scale-1,
+      y + (Circle_Size - 1) * AsConfig::Global_Scale -1 ,
+      x + 2 * AsConfig::Global_Scale, y + AsConfig::Global_Scale,
+      x + AsConfig::Global_Scale, y + 3 * AsConfig::Global_Scale);
+}
+//------------------------------------------------------------------------------------------------------------
+void AsPlatform::Draw_Normal_State(
+    HDC hdc, RECT &paint_area) { // Рисуем платформу в нормальном состоянии
+
+  int x = X_Pos;
+  int y = AsConfig::Platform_Y_Pos;
+
+  Clear_BG(hdc);
+
+  // 1. Рисуем боковые шарики
+  SelectObject(hdc, Platform_Circle_Pen);
+  SelectObject(hdc, Platform_Circle_Brush);
+
+  Ellipse(hdc, x * AsConfig::Global_Scale, y * AsConfig::Global_Scale,
+          (x + Circle_Size) * AsConfig::Global_Scale -1 ,
+          (y + Circle_Size) * AsConfig::Global_Scale -1 );
+  Ellipse(hdc, (x + Inner_Width) * AsConfig::Global_Scale,
+          y * AsConfig::Global_Scale,
+          (x + Circle_Size + Inner_Width) * AsConfig::Global_Scale -1 ,
+          (y + Circle_Size) * AsConfig::Global_Scale -1 );
+
+  // 2. Рисуем блик
+  Draw_Circle_Highlight(hdc, x * AsConfig::Global_Scale,
+                        y * AsConfig::Global_Scale);
+
+  // 3. Рисуем среднюю часть
+  SelectObject(hdc, Platform_Inner_Pen);
+  SelectObject(hdc, Platform_Inner_Brush);
+
+  RoundRect(hdc, (x + 4) * AsConfig::Global_Scale,
+            (y + 1) * AsConfig::Global_Scale,
+            (x + 4 + Inner_Width - 1) * AsConfig::Global_Scale -1 ,
+            (y + 1 + 5) * AsConfig::Global_Scale, 3 * AsConfig::Global_Scale -1 ,
+            3 * AsConfig::Global_Scale);
+}
+//------------------------------------------------------------------------------------------------------------
+void AsPlatform::Draw_Meltdown_State(
+    HDC hdc,
+    RECT &paint_area) { // Рисуем платформу в расплавленном состоянии
+
+  int i, j;
+  int x, y;
+  int y_offset;
+  int area_width, area_height;
+  int moved_columns_count = 0;
+  int max_platform_y;
+  COLORREF pixel;
+  COLORREF bg_pixel =
+      RGB(AsConfig::BG_Color.R, AsConfig::BG_Color.G, AsConfig::BG_Color.B);
+
+  area_width = Width * AsConfig::Global_Scale;
+  area_height = Height * AsConfig::Global_Scale + 1;
+
+  max_platform_y = AsConfig::Max_Y_Pos * AsConfig::Global_Scale + area_height;
+
+  for (i = 0; i < area_width; i++) {
+    if (Meltdown_Platform_Y_Pos[i] > max_platform_y)
+      continue;
+
+    ++moved_columns_count;
+
+    y_offset = AsConfig::Rand(Meltdown_Speed) + 1;
+    x = Platform_Rect.left + i;
+
+    for (j = 0; j < area_height; j++) {
+      y = Meltdown_Platform_Y_Pos[i] - j;
+
+      pixel = GetPixel(hdc, x, y);
+      SetPixel(hdc, x, y + y_offset, pixel);
+    }
+
+    for (j = 0; j < y_offset; j++) {
+      y = Meltdown_Platform_Y_Pos[i] - area_height + 1 + j;
+      SetPixel(hdc, x, y, bg_pixel);
+    }
+
+    Meltdown_Platform_Y_Pos[i] += y_offset;
+  }
+
+  if (moved_columns_count == 0)
+    Platform_State = EPS_Missing; // Вся платформа сдвинулась за пределы окна
+}
+//------------------------------------------------------------------------------------------------------------
+void AsPlatform::Draw_Roll_In_State(
+    HDC hdc, RECT &paint_area) { // Рисуем выкатывающуюся платформу
+
+  int x = X_Pos * AsConfig::Global_Scale;
+  int y = AsConfig::Platform_Y_Pos * AsConfig::Global_Scale;
+  int roller_size = Circle_Size * AsConfig::Global_Scale;
+  double alpha;
+  XFORM xform, old_xform;
+
+  Clear_BG(hdc);
+
+  // 1. Шарик
+  SelectObject(hdc, Platform_Circle_Pen);
+  SelectObject(hdc, Platform_Circle_Brush);
+
+  Ellipse(hdc, x, y, x + roller_size - 1, y + roller_size - 1);
+
+  // 2. Разделительная линия
+
+  alpha = -2.0 * M_PI / (double)Max_Rolling_Step * (double)Rolling_Step;
+
+  xform.eM11 = (float)cos(alpha);
+  xform.eM12 = (float)sin(alpha);
+  xform.eM21 = (float)-sin(alpha);
+  xform.eM22 = (float)cos(alpha);
+  xform.eDx = (float)(x + roller_size / 2.);
+  xform.eDy = (float)(y + roller_size / 2.);
+  GetWorldTransform(hdc, &old_xform);
+  SetWorldTransform(hdc, &xform);
+
+  SelectObject(hdc, AsConfig::BG_Pen);
+  SelectObject(hdc, AsConfig::BG_Brush);
+
+  Rectangle(hdc, -AsConfig::Global_Scale / 2, -roller_size / 2,
+            AsConfig::Global_Scale / 2 - 1, roller_size / 2 - 1);
+
+  SetWorldTransform(hdc, &old_xform);
+
+  // 3. Блик
+  Draw_Circle_Highlight(hdc, x, y);
+
+  ++Rolling_Step;
+
+  if (Rolling_Step >= Max_Rolling_Step)
+    Rolling_Step -= Max_Rolling_Step;
+
+  X_Pos -= Rolling_Platform_Speed;
+
+  if (X_Pos <= Roll_In_Platform_End_X_Pos) {
+    X_Pos += Rolling_Platform_Speed;
+    Platform_State = EPS_Expand_Roll_In;
+    Inner_Width = 1;
+  }
+}
+//------------------------------------------------------------------------------------------------------------
+void AsPlatform::Draw_Expanding_Roll_In_State(
+    HDC hdc,
+    RECT &paint_area) { // Рисуем расширяющуюся после выкатывания платформу
+
+  Draw_Normal_State(hdc, paint_area);
+
+  --X_Pos;
+  Inner_Width += 2;
+
+  if (Inner_Width >= Normal_Platform_Inner_Width) {
+    Inner_Width = Normal_Platform_Inner_Width;
+    Platform_State = EPS_Ready;
+    Redraw_Platform();
+  }
+}
+//------------------------------------------------------------------------------------------------------------
